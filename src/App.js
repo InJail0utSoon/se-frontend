cce2c3388da1b3fe78ddb8586c6b521a7902042c
Suraj Singh Rana <surajsinghrana7417@gmail.com>
2021-05-30 00:43:42 +0530

    code refactor and integrated with main search api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import {useState} from 'react'
 
 function App() {
 	let [state, setState] = useState({'page':'search-page', 'query':'', 'res':[]})
-	console.log("yo new state -> ", state)
+	console.log("rendering :: ", state)
+	
 	const search = (query) => {
-		fetch("http://localhost:8000/mock_search/"+query)
+		fetch("http://localhost:8000/search/"+query)
 		.then(res => res.json())
-		.then(data => setState(state => ({...state, 'page':'res-page', 'res':data})))
+		.then(data => setState(prevState => ({...prevState, 'page':'res-page', 'res':data})))
+		.catch(err => alert(err))
 	}
+
+	const updateSearchBar = (e) => setState(prevState => ({...prevState, 'query':e.target.value}))
 
 	if(state['page'] === "search-page") {
 		return (
@@ -19,7 +23,7 @@ function App() {
 						search(state['query'])
 					}
 				}>
-					<input value={state['query']} type="text" onChange={e => setState(state => ({...state, 'query':e.target.value}))} />
+					<input value={state['query']} type="text" onChange={updateSearchBar} />
 					<button type="submit" value="Submit" >
 						Search
 					</button>
@@ -39,18 +43,23 @@ function App() {
 							search(state['query'])
 						}
 					}>
-						<input value={state['query']} type="text" onChange={e => setState(state => ({...state, 'query':e.target.value}))} />
+						<input value={state['query']} type="text" onChange={updateSearchBar} />
 						<button type="submit" value="Submit" >
 							Search
 						</button>
 					</form>
 				</div>
 				<div>
-					{state['res'].map(elem => 
-						(<div>
-							<div>{elem.url}</div>
-							<div>{elem.content}</div>
-						</div>))
+					{
+						state['res'].map(elem => 
+							(<div>
+								<a href={elem.url}>{elem.url}</a>
+								<div>{elem.content}</div>
+								<div>
+									<div>{elem.timestamp}</div>
+									<div>{elem.score}</div>
+								</div>
+							</div>))
 					}
 				</div>
 			</div>
